3c70c812555c8d04ed5084de97214277da9bc655
flaribbit <1149761294@qq.com>
2020-11-12 00:30:33 +0800

    beautify

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
 const axios = require("axios").default;
 var candidates = [];
 var inputHandler = inputMusicURL;
+process.stdin.on("data", buffer => inputHandler(String(buffer)));
 
 function inputMusicURL(input) {
     var hash = input.match(/kugou.+hash=([0-9A-F]{32})/);
     if (hash) {
-        axios.get("http://krcs.kugou.com/search?ver=1&man=yes&hash=" + hash[1]).then(res => {
+        axios.get(`http://krcs.kugou.com/search?ver=1&man=yes&hash=${hash[1]}`).then(res => {
             if (res.data.info == "OK" && res.data.candidates.length > 0) {
                 candidates = res.data.candidates;
                 candidates.forEach((v, i) => console.log(`${i}. ${v.song}\t${v.singer}\t${v.nickname}`));
                 inputHandler = selectLyric;
+                console.log("请输入选项前数字");
             } else {
                 console.log("获取歌词失败，请检查音乐是否有歌词");
             }
         });
     } else {
-        console.log("请输入正确的连接");
+        console.log("请输入正确的歌曲链接");
     }
 }
 
@@ -23,7 +25,7 @@ function selectLyric(input) {
     var num = input.match(/(\d)/);
     if (num && num[1] < candidates.length) {
         var item = candidates[Number(num[1])];
-        axios.get("http://lyrics.kugou.com/download?ver=1&client=pc&id=" + item.id + "&accesskey=" + item.accesskey + "&fmt=krc&charset=utf8").then(res => {
+        axios.get(`http://lyrics.kugou.com/download?ver=1&client=pc&id=${item.id}&accesskey=${item.accesskey}&fmt=krc&charset=utf8`).then(res => {
             console.log(res.data);
         });
         inputHandler = inputMusicURL;
@@ -31,7 +33,3 @@ function selectLyric(input) {
         console.log("请输入正确的数字");
     }
 }
-
-process.stdin.on("data", (buffer) => {
-    inputHandler(String(buffer));
-});
